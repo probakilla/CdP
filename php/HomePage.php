867f530b71204b42f36dd2b203cc9de5ce2d89ad
Probak <probak@Debian.home>
2018-11-03 11:02:01 +0100

    Re structuration du projet

---
+++ php/HomePage.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Gestions de projets</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" type="text/css" href="../css/HomePageStyle.css">
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+                <div class="title m-b-md">
+                    Gestionnaire de projets
+                </div>
+                <div class="links">
+                    <a href="CreateProject.php">creer un nouveau projet</a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
